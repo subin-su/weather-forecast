b7a1961b7c73372ec8390c541f1ccab5a9eaa623
subinkc <subinkc140@gmail.com>
2021-01-16 21:47:52 -0800

    haha

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var inputValue=$("#input").trim().value;
-console.log(inputValue);
+
+var btnx=document.getElementById("btn")
+//console.log(inputy)
+btnx.addEventListener("click",function(e){
+    e.preventDefault();
+   let inputy=document.getElementById("inputx").value
+window.alert(inputy)
+  localStorage.setItem(inputy,"value");
+  let import=localStorage.getItem(inputy);
+  document.createElement(h2);
+  
+ 
+
+})
