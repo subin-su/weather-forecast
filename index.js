88107368e0818271f2d854d595ccefcc3c8515ec
subinkc <subinkc140@gmail.com>
2021-01-18 15:36:18 -0800

    added

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-
+var inputValue=document.getElementById("input")
+const apiKey="668e3d39d52a645e064391881ef3df01"
 var btnx=document.getElementById("btn")
-//console.log(inputy)
+var cityName=document.getElementById("temp")
+var sudan=document.getElementById("name")
+var subin=document.getElementById("population")
+var alina=document.getElementById("des")
+//var box=document.getElementById("")
 btnx.addEventListener("click",function(e){
-    e.preventDefault();
-   let inputy=document.getElementById("inputx").value
-window.alert(inputy)
-  localStorage.setItem(inputy,"value");
-  let import=localStorage.getItem(inputy);
-  document.createElement(h2);
-  
- 
+  e.preventDefault()
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputValue.value}&appid=${apiKey}`)
+.then(res=>res.json())
+.then(data=>
+{console.log(data)
 
+  let x =data["city"]["population"]
+  cityName.innerHTML=x;
+  let a=data["city"]["coord"]["lat"]
+  sudan.innerHTML=a;
+  let b=data["list"][0]["main"]["temp"]
+  subin.innerHTML=Math.floor((b-(273.15)+32)*2);
+
+})
 })
